8bc34d1246555241337d711c74e5804521d9cd35
Ryan Mansur <skywalker.ryan@gmail.com>
2025-03-10 13:05:31 -0400

    Linked List insertAtPos

--- Practices/LinkedList/index.js
+++ Practices/LinkedList/index.js
@@ -4,7 +4,10 @@ import { LinkedList } from "./linkedList.js";
 
 let list = new LinkedList();
 
-list.append(10);
 list.append(20);
+list.prepend(10);
 list.append(30);
+list.append(40);
+list.printList();
+list.insert(3, 25);
 list.printList();
--- Practices/LinkedList/linkedList.js
+++ Practices/LinkedList/linkedList.js
@@ -3,22 +3,18 @@ import { Node } from "./node.js";
 // const Node = require('./node')
 
 /*
+// append | insertAtTail
+// prepend | insertAtHead
+// insert | insertAtPos
 
-append
-insertAtHead
-insertAtTail
-insertAtPos
-
-delete
+// clearList
 deleteHead
 deleteTail
 deleteAtPos
 
 search
-printList
+// printList
 getSize
-
-
 */
 
 export class LinkedList
@@ -46,6 +42,48 @@ export class LinkedList
       current = current.next;
 
     current.next = newNode;
+  }
+
+  prepend (val)
+  {
+    const newNode = new Node(val);
+
+    this.size++;
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  insert (pos, val)
+  {
+    this.size++;
+
+    if (pos == 0)
+    {
+      this.prepend(val);
+      return;
+    }
+
+    const newNode = new Node(val);
+    let current = this.head;
+
+    for (let i = 1; i <= pos - 1; i++)
+    {
+      console.log("!!!!!!!!!!!!!! current : ", current);
+      console.log("!!!!!!!!!!!!!! newNode : ", newNode);
+      if (i == pos - 1)
+      {
+        newNode.next = current.next;
+        current.next = newNode;
+      }
+
+      current = current.next;
+    }
+  }
+
+  clearList ()
+  {
+    this.head = null;
+    this.size = 0;
   }
 
   printList ()
