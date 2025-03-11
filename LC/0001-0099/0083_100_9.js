251ff5b56f9f6e90ad703ec3ac8e44df50b0c515
Ryan Mansur <skywalker.ryan@gmail.com>
2025-03-10 20:29:59 -0400

    LC 0083

---
+++ LC/0001-0099/0083_100_9.js
@@ -0,0 +1,23 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head)
+{
+  let current = head;
+
+  while (current?.next)
+  {
+    if (current.val === current.next.val)
+      current.next = current.next.next
+    else
+      current = current.next
+  }
+
+  return head;
+};
+
+const head = [1, 1, 2];
+// const head = [1, 1, 2, 3, 3];
+
+console.log(deleteDuplicates(head));
--- Practices/LinkedList/index.js
+++ Practices/LinkedList/index.js
@@ -4,11 +4,11 @@ import { LinkedList } from "./linkedList.js";
 
 let list = new LinkedList();
 
+list.append(10);
 list.append(20);
-list.prepend(10);
 list.append(30);
 list.append(40);
-list.insert(3, 25);
-list.printList();
-list.deleteAtPos(2);
-list.printList();
+// list.insert(3, 25);
+// list.printList();
+// list.deleteAtPos(2);
+// list.printList();
