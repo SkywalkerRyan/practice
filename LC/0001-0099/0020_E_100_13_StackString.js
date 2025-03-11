52571adb7442d602cfcb42fea0e07e335079ae41
Ryan Mansur <skywalker.ryan@gmail.com>
2025-03-11 17:59:21 -0400

    LC 0020

---
+++ LC/0001-0099/0020_E_100_13_StackString.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s)
+{
+	let stack = [];
+
+	for (let i = 0; i < s.length; i++)
+	{
+		let _opposite = opposite(s[i]);
+
+		if (_opposite === 0)
+			stack.push(s[i]);
+		else
+		{
+			if (_opposite === stack[stack.length - 1])
+				stack.pop();
+			else
+				return false
+		}
+	}
+
+	if (stack.length === 0)
+		return true;
+
+	return false
+};
+
+let opposite = (s) =>
+{
+	switch (s)
+	{
+		case ")":
+			return "("
+		case "}":
+			return "{"
+		case "]":
+			return "["
+
+		default:
+			return 0;
+	}
+}
+
+// const testVal = "()"
+// const testVal = "()[]{}"
+// const testVal = "(]"
+// const testVal = "([])"
+const testVal = "("
+
+console.log(isValid(testVal))
+
