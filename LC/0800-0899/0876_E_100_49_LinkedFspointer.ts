aacb6fffbd8022a64bb3b6f3ea339b4c6a5ca386
Ryan Mansur <skywalker.ryan@gmail.com>
2025-10-19 20:18:23 -0400

    LC 0876

---
+++ LC/0800-0899/0876_E_100_49_LinkedFspointer.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+import { Node as ListNode } from '../../Practices/LinkedList/node.js';
+
+function middleNode(head: ListNode | null): ListNode | null {
+	if (!head.next) return head;
+
+	let fast: ListNode = head;
+	let slow: ListNode = head;
+
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+
+	return slow;
+}
---
+++ Striver/6_3.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+import { Node as ListNode } from '../Practices/LinkedList/node.js';
+
+// function middleNode(head: ListNode | null): ListNode | null {
+// 	if (!head.next) return head;
+
+// 	let count: number = 1;
+// 	let curr = head;
+
+// 	while (curr.next) {
+// 		count++;
+// 		curr = curr.next;
+// 	}
+
+// 	if (count === 2) return head.next;
+
+// 	curr = head;
+// 	count = Math.floor(count / 2);
+// 	let _count: number = 0;
+// 	while (curr.next) {
+// 		_count++;
+// 		if (_count > count) return curr;
+// 		curr = curr.next;
+// 	}
+// }
+
+function middleNode(head: ListNode | null): ListNode | null {
+	if (!head.next) return head;
+
+	let fast: ListNode = head;
+	let slow: ListNode = head;
+
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+
+	return slow;
+}
