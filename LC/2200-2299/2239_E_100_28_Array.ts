5802db1d241381b815139825b7fc413698ff859c
Ryan Mansur <skywalker.ryan@gmail.com>
2025-10-23 15:24:55 -0400

    LC 2239

---
+++ LC/2200-2299/2239_E_100_28_Array.ts
@@ -0,0 +1,18 @@
+function findClosestNumber(nums: number[]): number {
+	let closest = nums[0];
+
+	for (let i = 0; i < nums.length; i++) {
+		if (Math.abs(nums[i]) < Math.abs(closest)) {
+			closest = nums[i];
+		}
+		if (Math.abs(nums[i]) === Math.abs(closest)) {
+			closest = Math.max(nums[i], closest);
+		}
+	}
+
+	return closest;
+}
+
+console.log(findClosestNumber([2, 1, 1, -1, 100000]));
+// console.log(findClosestNumber([2, -1, 1]));
+// console.log(findClosestNumber([-100000, -100000]));
