6dd2c8570e487952c1547dc608bce7166b5b3d77
Ryan Mansur <skywalker.ryan@gmail.com>
2025-10-27 00:54:24 -0400

    LC 0167

--- AlgoMap/03_Twopoint.ts
+++ AlgoMap/03_Twopoint.ts
@@ -35,3 +35,16 @@ function reverseString(s: string[]): void {
 		right--;
 	}
 }
+
+function twoSum(numbers: number[], target: number): number[] {
+	let left = 0;
+	let right = numbers.length - 1;
+
+	while (left < right) {
+		if (numbers[left] + numbers[right] === target) return [left + 1, right + 1];
+		else if (numbers[left] + numbers[right] > target) right--;
+		else left++;
+	}
+
+	return [];
+}
