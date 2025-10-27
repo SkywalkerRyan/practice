f99c4db58fb228ef85135e1487568b25b413c237
Ryan Mansur <skywalker.ryan@gmail.com>
2025-10-26 21:54:37 -0400

    LC 0217

--- AlgoMap/02_Hash.ts
+++ AlgoMap/02_Hash.ts
@@ -14,3 +14,15 @@ function numJewelsInStones(jewels: string, stones: string): number {
 }
 
 console.log(numJewelsInStones('aA', 'aAAbbbb'));
+
+function containsDuplicate(nums: number[]): boolean {
+	let map = new Map();
+
+	for (let i = 0; i < nums.length; i++) {
+		if (map.has(nums[i])) return true;
+
+		map.set(nums[i], 1);
+	}
+
+	return false;
+}
