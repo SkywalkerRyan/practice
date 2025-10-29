9f6b5f57c283860c8149fe2df6b542fa495ddd86
Ryan Mansur <skywalker.ryan@gmail.com>
2025-10-29 00:05:16 -0400

    Striver Stack using Linked List

--- Striver/09_Stack_Queue/9_1.ts
+++ Striver/09_Stack_Queue/9_1.ts
@@ -62,9 +62,75 @@ class Queue {
 	}
 }
 
-let queue = new Queue();
+// let queue = new Queue();
 
-queue.push(5);
-console.log(queue.top());
-console.log(queue.size());
-console.log(queue.pop());
+// queue.push(5);
+// console.log(queue.top());
+// console.log(queue.size());
+// console.log(queue.pop());
+
+import { Node } from '../../Practices/LinkedList/node.js';
+class StacksL {
+	head: Node | null;
+	length: number;
+
+	constructor() {
+		this.head = null;
+		this.length = 0;
+	}
+
+	push(val): void {
+		let temp = new Node(val);
+		this.length++;
+
+		if (this.length === 1) {
+			this.head = temp;
+			return;
+		}
+
+		let curr = this.head;
+		while (curr.next) {
+			curr = curr.next;
+		}
+		curr.next = temp;
+	}
+
+	pop(): Node {
+		let prev = this.head;
+		let curr = this.head;
+		this.length--;
+
+		while (curr.next) {
+			prev = curr;
+			curr = curr.next;
+		}
+		prev.next = null;
+
+		return curr;
+	}
+
+	top(): Node {
+		let curr = this.head;
+
+		while (curr.next) {
+			curr = curr.next;
+		}
+
+		return curr;
+	}
+
+	isEmpty(): boolean {
+		return this.length === 0;
+	}
+
+	size(): number {
+		return this.length;
+	}
+}
+
+let stack = new StacksL();
+
+stack.push(6);
+console.log(stack.top());
+console.log(stack.size());
+console.log(stack.pop());
