4a360e439068c894d6057db1e4cbed3a86dbc3e1
Ryan Mansur <skywalker.ryan@gmail.com>
2025-10-31 01:34:29 -0400

    Striver Next Smaller Element

--- Striver/09_Stack_Queue/9_3.ts
+++ Striver/09_Stack_Queue/9_3.ts
@@ -21,7 +21,7 @@ function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
 	return res;
 }
 
-console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
+// console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
 
 function nextGreaterElements(nums: number[]): number[] {
 	let stk = [];
@@ -43,3 +43,24 @@ function nextGreaterElements(nums: number[]): number[] {
 
 	return res;
 }
+
+function nextSmallerElement(nums: number[]): number[] {
+	let map = new Map();
+	let stack = [];
+	let res = [];
+
+	for (let i = nums.length - 1; i >= 0; i--) {
+		while (stack.length && stack[stack.length - 1] > nums[i]) {
+			stack.pop();
+		}
+
+		if (stack.length === 0) res[i] = -1;
+		else res[i] = stack[stack.length - 1];
+
+		stack.push(nums[i]);
+	}
+
+	return res;
+}
+
+console.log(nextSmallerElement([1, 3, 4, 2]));
