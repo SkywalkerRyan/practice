76a310c1e4cac68f2ec600c551f34e5849d94057
Ryan Mansur <skywalker.ryan@gmail.com>
2025-11-09 22:04:59 -0500

    LC 1004

---
+++ LC/1000-1099/1004_M_84_92_TwpArr.ts
@@ -0,0 +1,23 @@
+function longestOnes(nums: number[], k: number): number {
+	let left = 0;
+	let right = 0;
+
+	let zero = 0;
+	let res = 0;
+
+	while (right < nums.length) {
+		nums[right] === 0 ? zero++ : null;
+
+		while (zero > k) {
+			nums[left] === 0 ? zero-- : null;
+			left++;
+		}
+		right++;
+		res = Math.max(res, right - left);
+	}
+
+	return res;
+}
+
+// console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2));
+console.log(longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3));
