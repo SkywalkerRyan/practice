5047302386ffb93743477051a625fb45cc37509a
Ryan Mansur <skywalker.ryan@gmail.com>
2025-11-09 23:11:52 -0500

    LC 0424

--- LC/1000-1099/1004_M_84_92_TwpArr.ts
+++ LC/1000-1099/1004_M_84_92_TwpArr.ts
@@ -43,3 +43,24 @@ function totalFruits(fruits: number[]) {
 
 	return res;
 }
+
+function characterReplacement(s: string, k: number): number {
+	let left = 0;
+
+	let map = new Map();
+	let res = 0;
+	let max = 0;
+
+	for (let right = 0; right < s.length; right++) {
+		map.set(s[right], (map.get(s[right]) || 0) + 1);
+		max = Math.max(max, map.get(s[right]));
+
+		while (right - left + 1 - max > k) {
+			map.set(s[left], map.get(s[left]) - 1);
+			left++;
+		}
+		res = Math.max(res, right - left + 1);
+	}
+
+	return res;
+}
