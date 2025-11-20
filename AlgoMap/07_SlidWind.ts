61c6941b2cbe3376b3d1c7a50a48e74e92159448
Ryan Mansur <skywalker.ryan@gmail.com>
2025-11-19 23:58:08 -0500

    LC 0209

--- AlgoMap/07_SlidWind.ts
+++ AlgoMap/07_SlidWind.ts
@@ -61,5 +61,29 @@ function findMaxAverage(nums: number[], k: number): number {
 	return res / k;
 }
 
-console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4));
-console.log(findMaxAverage([5], 1));
+// console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4));
+// console.log(findMaxAverage([5], 1));
+
+// 0209
+function minSubArrayLen(target: number, nums: number[]): number {
+	let res = Infinity;
+	let win = 0;
+
+	let left = 0;
+	for (let right = 0; right < nums.length; right++) {
+		win += nums[right];
+
+		while (win >= target) {
+			res = Math.min(res, right - left + 1);
+			win -= nums[left];
+			left++;
+		}
+	}
+
+	return res === Infinity ? 0 : res;
+}
+
+// console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])); // 2
+// console.log(minSubArrayLen(4, [1, 4, 4]));
+// console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1]));
+// console.log(minSubArrayLen(11, [1, 2, 3, 4, 5])); // 3
