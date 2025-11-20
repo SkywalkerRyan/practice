53ba0f1860366fc787da80aac3617c278dc9f387
Ryan Mansur <skywalker.ryan@gmail.com>
2025-11-20 11:46:50 -0500

    LC 0150

--- LC/0100-0199/0150_M_51_53_StkArr.ts
+++ LC/0100-0199/0150_M_51_53_StkArr.ts
@@ -1,25 +1,5 @@
-// 682
-function calPoints(operations: string[]): number {
-	let res = 0;
-	let stk: number[] = [];
+// AlgoMap Stk 04.3
 
-	for (const item of operations) {
-		if (!isNaN(Number(item))) stk.push(Number(item));
-		if (item === 'C') stk.pop();
-		if (item === 'D') stk.push(stk[stk.length - 1] * 2);
-		if (item === '+') stk.push(stk[stk.length - 1] + stk[stk.length - 2]);
-	}
-
-	for (const j of stk) {
-		res += j;
-	}
-
-	return res;
-}
-
-// console.log(calPoints(['5', '2', 'C', 'D', '+']));
-
-// 0150
 function evalRPN(tokens: string[]): number {
 	let stk = [];
 
@@ -33,7 +13,7 @@ function evalRPN(tokens: string[]): number {
 				stk.push(stk.pop() - min);
 				break;
 			case '*':
-				stk.push(Math.trunc(stk.pop() * stk.pop()));
+				stk.push(stk.pop() * stk.pop());
 				break;
 			case '/':
 				let div = stk.pop();
