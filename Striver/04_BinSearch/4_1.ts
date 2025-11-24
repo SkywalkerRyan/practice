d491a1bdc4629653c8eea0b2bbeb6b0345d714a9
Ryan Mansur <skywalker.ryan@gmail.com>
2025-11-24 16:35:07 -0500

    LC 0034

--- Striver/04_BinSearch/4_1.ts
+++ Striver/04_BinSearch/4_1.ts
@@ -108,3 +108,33 @@ function getFloorAndCeil(nums: number[], target: number): number[] {
 	return [nums[res], nums[res + 1]];
 }
 // console.log(getFloorAndCeil([3, 4, 4, 7, 8, 10], 5));
+
+// 0034
+function searchRange(nums: number[], target: number): number[] {
+	let leftRes = searchOccurence(nums, target, true);
+	let rightRes = searchOccurence(nums, target, false);
+
+	return [leftRes, rightRes];
+}
+
+function searchOccurence(nums: number[], target: number, isSearchingLeft: boolean): number {
+	let res = -1;
+	let left = 0;
+	let right = nums.length - 1;
+
+	while (left <= right) {
+		let mid = Math.floor((left + right) / 2);
+
+		if (nums[mid] > target) right = mid - 1;
+		else if (nums[mid] < target) left = mid + 1;
+		else {
+			res = mid;
+			if (isSearchingLeft) right = mid - 1;
+			else left = mid + 1;
+		}
+	}
+
+	return res;
+}
+// console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
+console.log(searchRange([5, 7, 7, 8, 8, 10], 10));
