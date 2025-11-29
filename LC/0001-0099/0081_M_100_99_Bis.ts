ad1821a4bd5c3e4d89a5b54b2c45749391e201d4
Ryan Mansur <skywalker.ryan@gmail.com>
2025-11-28 23:01:06 -0500

    LC 0081

--- LC/0001-0099/0081_M_100_99_Bis.ts
+++ LC/0001-0099/0081_M_100_99_Bis.ts
@@ -1,30 +1,3 @@
-// Striver 4.1.8
-function search(nums: number[], target: number): number {
-	let left = 0;
-	let right = nums.length - 1;
-
-	while (left <= right) {
-		let mid = Math.floor((left + right) / 2);
-
-		if (nums[mid] === target) return mid;
-
-		if (nums[mid] >= nums[left]) {
-			if (nums[left] <= target && nums[mid] > target) {
-				right = mid - 1;
-			} else {
-				left = mid + 1;
-			}
-		} else {
-			if (nums[right] >= target && nums[mid] < target) {
-				left = mid + 1;
-			} else right = mid - 1;
-		}
-	}
-
-	return -1;
-}
-// console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
-
 // 0081
 function search(nums: number[], target: number): number {
 	let left = 0;
