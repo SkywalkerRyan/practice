3fd1d96386730cc4a472ec14d066b6e0d2a54a19
Ryan Mansur <skywalker.ryan@gmail.com>
2025-12-16 23:28:07 -0500

    LC 0069

---
+++ Striver/04_BinSearch/4_2.ts
@@ -0,0 +1,19 @@
+// 0069
+function floorSqrt(x: number): number {
+	if (x < 2) return x;
+
+	let low = 1;
+	let high = x;
+
+	while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+
+		if (mid * mid === x) return mid;
+		else if (mid * mid < x) {
+			low = mid + 1;
+		} else high = mid - 1;
+	}
+
+	return high;
+}
+// console.log(floorSqrt(8));
