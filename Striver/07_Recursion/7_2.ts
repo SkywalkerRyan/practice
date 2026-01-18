313fc17619c678a52ddacbda01c5506af635c97e
Ryan Mansur <skywalker.ryan@gmail.com>
2026-01-18 18:21:06 -0500

    LC 0040

--- Striver/07_Recursion/7_2.ts
+++ Striver/07_Recursion/7_2.ts
@@ -131,4 +131,40 @@ function backtrackComSum(
 	backtrackComSum(cur, curSum + nums[curIdx], curIdx, res, nums, target);
 	cur.pop();
 }
-console.log(combinationSum([2, 3, 6, 7], 7));
+// console.log(combinationSum([2, 3, 6, 7], 7));
+
+// 0040
+function combinationSum2(candidates: number[], target: number): number[][] {
+	let res = [];
+	candidates = candidates.sort();
+
+	backtrackComSum2([], 0, 0, candidates, res, target);
+
+	return res;
+}
+
+function backtrackComSum2(
+	cur: number[],
+	curIdx: number,
+	curSum: number,
+	nums: number[],
+	res: number[][],
+	target: number
+): void {
+	if (curSum === target) {
+		res.push([...cur]);
+		return;
+	}
+
+	if (curSum > target || curIdx >= nums.length) return;
+
+	for (let i = curIdx; i < nums.length; i++) {
+		if (i > curIdx && nums[i] === nums[i - 1]) continue;
+
+		cur.push(nums[i]);
+		backtrackComSum2(cur, i + 1, curSum + nums[i], nums, res, target);
+		cur.pop();
+	}
+}
+
+// console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8));
