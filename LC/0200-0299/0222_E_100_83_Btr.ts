cab0533bf8cb8c79ef179e5ad5f6332e8f26105e
Ryan Mansur <skywalker.ryan@gmail.com>
2026-01-29 12:58:51 -0500

    LC 0222

---
+++ LC/0200-0299/0222_E_100_83_Btr.ts
@@ -0,0 +1,22 @@
+// Striver 13.3.6
+function countNodes(root: TreeNode | null): number {
+	if (root === null) return 0;
+
+	let left = root.left ? root.left : null;
+	let right = root.right ? root.right : null;
+	let leftHeight = 1;
+	let rightHeight = 1;
+
+	while (left != null) {
+		leftHeight++;
+		left = left.left;
+	}
+	while (right != null) {
+		rightHeight++;
+		right = right.right;
+	}
+
+	if (leftHeight === rightHeight) return 2 ** leftHeight - 1;
+
+	return 1 + countNodes(root.left) + countNodes(root.right);
+}
--- Striver/13_BinTrees/13_3.ts
+++ Striver/13_BinTrees/13_3.ts
@@ -233,3 +233,26 @@ function timeToBurn(root: TreeNode | null, start: TreeNode | null): number {
 
 	return distance - 1;
 }
+
+// 0222
+function countNodes(root: TreeNode | null): number {
+	if (root === null) return 0;
+
+	let left = root.left ? root.left : null;
+	let right = root.right ? root.right : null;
+	let leftHeight = 1;
+	let rightHeight = 1;
+
+	while (left != null) {
+		leftHeight++;
+		left = left.left;
+	}
+	while (right != null) {
+		rightHeight++;
+		right = right.right;
+	}
+
+	if (leftHeight === rightHeight) return 2 ** leftHeight - 1;
+
+	return 1 + countNodes(root.left) + countNodes(root.right);
+}
