a563e682bf47a269c18db9f9690e6be283166e9c
Ryan Mansur <skywalker.ryan@gmail.com>
2026-01-30 12:07:58 -0500

    LC 0106

---
+++ LC/0100-0199/0106_M_91_51_BtrRec.ts
@@ -0,0 +1,34 @@
+// Striver 13.3.10
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+	let inMap = new Map<number, number>();
+
+	for (let i = 0; i < inorder.length; i++) {
+		inMap.set(inorder[i], i);
+	}
+
+	let cur = buildHelper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, inMap);
+
+	return cur;
+}
+
+function buildHelper(
+	inOrder: number[],
+	inStart: number,
+	inEnd: number,
+	postOrder: number[],
+	postStart: number,
+	postEnd: number,
+	inMap: Map<number, number>
+): TreeNode | null {
+	if (inStart > inEnd || postStart > postEnd) return null;
+
+	let node = new TreeNode(postOrder[postEnd]);
+
+	let inRoot = inMap.get(node.val);
+	let leftNums = inRoot - inStart;
+
+	node.left = buildHelper(inOrder, inStart, inRoot - 1, postOrder, postStart, postStart + leftNums - 1, inMap);
+	node.right = buildHelper(inOrder, inRoot + 1, inEnd, postOrder, postStart + leftNums, postEnd - 1, inMap);
+
+	return node;
+}
