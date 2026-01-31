86bf77c1d35d18de67ac4247fbf46f335f6c6c8d
Ryan Mansur <skywalker.ryan@gmail.com>
2026-01-30 22:46:17 -0500

    LC 0701 recursive

---
+++ LC/0700-0799/0701_M_100_77_BstRec.ts
@@ -0,0 +1,9 @@
+// 0701
+function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+	if (root === null) return new TreeNode(val);
+
+	if (val < root.val) root.left = insertIntoBST(root.left, val);
+	else root.right = insertIntoBST(root.right, val);
+
+	return root;
+}
