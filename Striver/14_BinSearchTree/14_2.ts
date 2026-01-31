99f33e966e8a86c883169f1213e2cd51c62b2b3c
Ryan Mansur <skywalker.ryan@gmail.com>
2026-01-30 22:19:08 -0500

    Striver 14.2.1

---
+++ Striver/14_BinSearchTree/14_2.ts
@@ -0,0 +1,30 @@
+// Striver 14.2.1
+function ceiling(root: TreeNode | null, target: number): TreeNode | null {
+	if (root === null) return root;
+
+	let res = { ceil: null };
+
+	return ceilingHelper(root, target, res);
+}
+
+function ceilingHelper(node: TreeNode | null, target: number, res: { ceil: number | null }): TreeNode | null {
+	if (node === null) return node;
+
+	if (node.val === target) return node;
+	else if (node.val < target) return ceilingHelper(node.right, target, res);
+	else {
+		res.ceil = node;
+		return ceilingHelper(node.left, target, res);
+	}
+}
+
+// Striver 14.2.1
+function ceiling(root: TreeNode | null, target: number): TreeNode | null {
+	if (root === null) return root;
+
+	if (root.val === target) return root;
+	if (root.val < target) return ceiling(root.right, target);
+
+	let ceil = ceiling(root.left, target);
+	return ceil !== null ? ceil : root;
+}
